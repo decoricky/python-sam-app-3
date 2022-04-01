d4c71ab8ba07de2fadd54c37f03b24473e2eef53
decoricky <rikiya.shikida@gmail.com>
2022-04-01 21:20:54 +0900

    fix ttl attribute

--- sam/bmonster/app/models.py
+++ sam/bmonster/app/models.py
@@ -13,54 +13,66 @@ class BaseMeta:
     billing_mode = os.getenv('BILLING_MODE')
 
 
-class Performer(Model):
+class PerformerIndex(Model):
     class Meta(BaseMeta):
         table_name = 'performer'
 
     name = UnicodeAttribute(hash_key=True)
-    # ttl = TTLAttribute(default=datetime.now(JST) + timedelta(days=31))
 
     def __str__(self):
         return self.name
 
 
-class Program(Model):
+class Performer(PerformerIndex):
+    ttl = TTLAttribute(default=datetime.now(JST) + timedelta(days=31))
+
+
+class ProgramIndex(Model):
     class Meta(BaseMeta):
         table_name = 'program'
 
     performer = UnicodeAttribute(hash_key=True)
     vol = UnicodeAttribute(range_key=True)
-    # ttl = TTLAttribute(default=datetime.now(JST) + timedelta(days=31))
 
 
-class Schedule(Model):
+class Program(ProgramIndex):
+    ttl = TTLAttribute(default=datetime.now(JST) + timedelta(days=31))
+
+
+class ScheduleIndex(Model):
     class Meta(BaseMeta):
         table_name = 'schedule'
 
     performer = UnicodeAttribute(hash_key=True)
     vol = UnicodeAttribute(range_key=True)
+
+
+class Schedule(ScheduleIndex):
     schedule_list = ListAttribute()
-    # ttl = TTLAttribute(default=datetime.now(JST) + timedelta(days=1))
+    ttl = TTLAttribute(default=datetime.now(JST) + timedelta(days=1))
 
 
-class ProgramReview(Model):
+class ProgramReviewIndex(Model):
     class Meta(BaseMeta):
         table_name = 'program_review'
 
     performer = UnicodeAttribute(hash_key=True)
     vol = UnicodeAttribute(range_key=True)
+
+
+class ProgramReview(ProgramIndex):
     star = NumberAttribute()
 
 
 def dynamodb_migrate():
-    if not Performer.exists():
-        Performer.create_table()
+    if not PerformerIndex.exists():
+        PerformerIndex.create_table()
 
-    if not Program.exists():
-        Program.create_table()
+    if not ProgramIndex.exists():
+        ProgramIndex.create_table()
 
-    if not Schedule.exists():
-        Schedule.create_table()
+    if not ScheduleIndex.exists():
+        ScheduleIndex.create_table()
 
-    if not ProgramReview.exists():
-        ProgramReview.create_table()
+    if not ProgramReviewIndex.exists():
+        ProgramReviewIndex.create_table()
